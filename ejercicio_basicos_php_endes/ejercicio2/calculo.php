06ba86034d77a3aa30818d25a3917a921985a703
JuanJoseFernandez10 <fernandez.suarez.juan.jose@iescamas.es>
2024-10-04 11:14:21 +0200

    Add files via upload

---
+++ ejercicio_basicos_php_endes/ejercicio2/calculo.php
@@ -0,0 +1,9 @@
+<?PHP
+
+$distancia = $_GET ['distancia'];
+$velocidad = $_GET['velocidad'];
+$tiempo = $distancia / $velocidad;
+echo "Hola el tiempo que tardaras ". " $tiempo horas";
+
+?><br>
+<a href="formulario.html">Atras</a>
